2c31af6c46ad6c79d11e1d3c67ccbd1fa9bafa75
Russell Smith <russ@rainforestqa.com>
2017-07-06 15:52:08 -0700

    LOL

--- memer.rb
+++ memer.rb
@@ -4,6 +4,18 @@ class Memer < SlackRubyBot::Bot
   command 'ping' do |client, data, match|
     client.say(text: 'pong', channel: data.channel)
   end
+
+  command 'what time is it?' do |client, data, match|
+    client.say(text: Time.now, channel: data.channel)
+  end
+
+  command 'puppy-me' do |client, data, match|
+    client.say(gif: 'puppy', channel: data.channel)
+  end
+
+  command 'cheer me up' do |client, data, match|
+    client.say(gif: 'happy', channel:data.channel)
+  end
 end
 
 Memer.run
